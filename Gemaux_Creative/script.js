c5e2d16b7b78d21de70d9a81d4c53c5fe099c2b9
Erick <114015229+khantako3132@users.noreply.github.com>
2023-09-29 10:24:48 +0630

    Add files via upload

---
+++ Gemaux_Creative/script.js
@@ -0,0 +1,9 @@
+function openMenu(){
+    document.getElementById('links').style.width = "150px"
+    document.getElementById('bars').style.display = "none"
+    
+}
+function closeMenu(){
+    document.getElementById('links').style.width = "0px"
+    document.getElementById('bars').style.display = "block"
+}
